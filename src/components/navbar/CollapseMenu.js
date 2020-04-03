a3631da1db7583311510893f04a976910b6b3d06
Eduardo Esteban <eduardoe@stanford.edu>
2020-04-02 19:14:49 -0500

    Working dynamic navbar, needs content

---
+++ src/components/navbar/CollapseMenu.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { useSpring, animated } from 'react-spring';
+
+const CollapseMenu = (props) => {
+  const { open } = useSpring({ open: props.navbarState ? 0 : 1 });
+
+  if (props.navbarState === true) {
+    return (
+      <CollapseWrapper style={{
+        transform: open.interpolate({
+          range: [0, 0.2, 0.3, 1],
+          output: [0, -20, 0, -200],
+        }).interpolate(openValue => `translate3d(0, ${openValue}px, 0`),
+      }}
+      >
+        <NavLinks>
+          <li><a href="/" onClick={props.handleNavbar}>Home</a></li>
+          <li><a href="/" onClick={props.handleNavbar}>About Us</a></li>
+          <li><a href="/" onClick={props.handleNavbar}>Our Leadership</a></li>
+          <li><a href="/" onClick={props.handleNavbar}>Our Events</a></li>
+          <li><a href="/" onClick={props.handleNavbar}>Hermano of the Week</a></li>
+          <li><a href="/" onClick={props.handleNavbar}>Scholarship</a></li>
+          <li><a href="/" onClick={props.handleNavbar}>Constitution</a></li>
+        </NavLinks>
+      </CollapseWrapper>
+    );
+  }
+  return null;
+};
+
+export default CollapseMenu;
+
+const CollapseWrapper = styled(animated.div)`
+  background: #2d3436;
+  position: fixed;
+  top: 4.5rem;
+  left: 0;
+  right: 0;
+  @media (min-width: 970px) {
+   display: none;
+  }
+`;
+
+const NavLinks = styled.ul`
+  list-style-type: none;
+  padding: 2rem 1rem 2rem 2rem;
+
+  & li {
+    transition: all 300ms linear 0s;
+  }
+
+  & a {
+    font-size: 1.4rem;
+    line-height: 2;
+    color: #dfe6e9;
+    text-decoration: none;
+    cursor: pointer;
+
+    &:hover {
+      color: #fdcb6e;
+      border-bottom: 1px solid #fdcb6e;
+    }
+  }
+
+  }
+`;
